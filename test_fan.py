bab18420f0d3be16d78e52b85159df673887e73c
conradjosesalayog <radradsalayog@gmail.com>
2026-05-30 01:36:37 +0800

    Added some colors and labels to the FIRST FAN. Fixed structure

--- test_fan.py
+++ test_fan.py
@@ -1,9 +1,11 @@
 from encapsulated_fan_class import Fan
 
+#ANSI COLOR CODES
 reset = "\033[0m"
 red = "\033[31m"
 green = "\033[32m"
 yellow = "\033[33m"
+orange = "\033[38;5;208m"
 blue = "\033[34m"
 cyan = "\033[36m"
 bold = "\033[1m"
@@ -20,13 +22,13 @@ def main():
     #second_fan will be the default
     second_fan = Fan()
 
-    print("FIRST FAN")
-    print(first_fan.get_speed())
-    print(first_fan.get_radius())
-    print(first_fan.get_color())
-    print(first_fan.get_on())
+    print(bold + cyan + "FIRST FAN" + reset)
+    print(red + "Speed  :", first_fan.get_speed(), reset)
+    print(green + "Radius :", first_fan.get_radius(), reset)
+    print(yellow + "Color  :", first_fan.get_color(), reset)
+    print(orange + "On     :", first_fan.get_on(), reset)
 
-    print("---------------------------------------------")
+    print(f"-------------------{bold}CONRAD MAANGAS FANS{reset}-------------------------")
 
     print("SECOND FAN")
     print(second_fan.get_speed())
